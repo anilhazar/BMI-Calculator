4c696f5d24b19efb642d4c260378c673360e410c
Ali Anıl Hazar <112640395+anilhazar@users.noreply.github.com>
2023-07-07 17:18:31 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,14 +8,12 @@ app.get("/", function(req,res){
   res.sendFile(__dirname + "/index.html");
 })
 
-
 app.post("/", function(req,res){
   let height1 = Number(req.body.height);
   let weight1 =  Number(req.body.weight);
   let result = parseInt(weight1/(height1 * height1));
   res.send("Your BMI score is " + result );
 })
-
 
 app.listen(3000, function(){
     console.log("Server 3000 is on progress");
